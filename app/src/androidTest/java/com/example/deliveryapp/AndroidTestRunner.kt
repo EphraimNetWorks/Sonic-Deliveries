dbd6dfddc9d8a15015308bb08e4ea540464d19ca
Ephraim Nartey <narteyephraim@gmail.com>
2020-01-01 22:00:16 +0000

    crashlytics dependency fix

--- app/src/androidTest/java/com/example/deliveryapp/AndroidTestRunner.kt
+++ app/src/androidTest/java/com/example/deliveryapp/AndroidTestRunner.kt
@@ -3,14 +3,11 @@ package com.example.deliveryapp
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
-import com.github.tmurakami.dexopener.DexOpener
 
 class AndroidTestRunner :  AndroidJUnitRunner(){
 
     override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
-        // The DexOpener library allows to make Kotlin classes, properties and functions
-        // open during tests in order to mock then with Mockito.
-        DexOpener.install(this)
+
         return super.newApplication(cl, com.example.deliveryapp.AndroidTestApplication::class.java.name, context)
     }
 }
--- app/src/main/java/com/example/deliveryapp/utils/ReleaseTree.kt
+++ app/src/main/java/com/example/deliveryapp/utils/ReleaseTree.kt
@@ -1,9 +1,10 @@
 package com.example.deliveryapp.utils
 
+import com.crashlytics.android.Crashlytics
 import timber.log.Timber
 
 class ReleaseTree : Timber.Tree(){
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-
+        Crashlytics.log(priority, tag, message)
     }
 }
